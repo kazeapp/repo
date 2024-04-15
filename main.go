cdd164a95525afbc1ace1a5bff2b1976d2bfe288
kazeapp <kazeapp2022@gmail.com>
2024-04-15 16:35:33 +0545

    added auto repo.json gen... thanks to miru repo

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"path"
+	"regexp"
+	"strings"
+)
+
+func main() {
+	extensions := readRepoExtensions()
+	f, err := os.Create("repo.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	b, err := json.MarshalIndent(extensions, "", " ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.Write(b)
+}
+
+func readRepoExtensions() []map[string]string {
+	de, err := os.ReadDir("repo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var extensions []map[string]string
+	for _, de2 := range de {
+		b, err := os.ReadFile(path.Join("repo", de2.Name()))
+		if err != nil {
+			log.Println("error:", err)
+			continue
+		}
+		r, _ := regexp.Compile(`KazeExtension([\s\S]+?)/KazeExtension`)
+		data := r.FindAllString(string(b), -1)
+		if len(data) < 1 {
+			log.Println("error: not extension")
+			continue
+		}
+		lines := strings.Split(data[0], "\n")
+		extension := make(map[string]string)
+		for _, v := range lines {
+			if v[:4] == "// @" {
+				s := strings.Split(v[4:], " ")
+				extension[s[0]] = strings.Trim(s[len(s)-1], "\r")
+			}
+		}
+		extension["url"] = de2.Name()
+		extensions = append(extensions, extension)
+	}
+	return extensions
+}
