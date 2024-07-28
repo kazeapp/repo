00697da1e407191a86dc358d869bb2c45adeab0a
kazeapp <kazeapp2022@gmail.com>
2024-07-29 02:55:07 +0545

    hiperdex

--- repo/javascript/manga/multisrc/madara/madara.mjs
+++ repo/javascript/manga/multisrc/madara/madara.mjs
@@ -5,15 +5,55 @@ class Madara extends KProvider {
   //   this.lang = lang;
   // }
 
+  siteConfig() {
+    return {
+      hasLatest: true,
+      hasPopular: true,
+      hasTVLatest: false,
+      hasTVPopular: false,
+      dateFormat: "",
+    };
+  }
+
   async getPopular(page) {
     const res = (
       await new Client().get(
         `${this.extension.baseUrl}/manga/page/${page}/?m_orderby=views`
       )
     ).body;
-
     const document = new Document(res);
     return mangaFromElements(document.select("div.page-item-detail"));
+  }
+
+  async getLatestUpdates(page) {
+    const res = (
+      await new Client().get(
+        `${this.extension.baseUrl}/manga/page/${page}/?m_orderby=latest`
+      )
+    ).body;
+    const document = new Document(res);
+    return mangaFromElements(document.select("div.page-item-detail"));
+  }
+
+  async search(query, page, filters) {
+    throw new Error("search not implemented");
+  }
+  async getDetail(url) {
+    throw new Error("getDetail not implemented");
+  }
+  // For anime episode video list
+  async getVideoList(url) {
+    throw new Error("getVideoList not implemented");
+  }
+  // For manga chapter pages
+  async getPageList() {
+    throw new Error("getPageList not implemented");
+  }
+  getFilterList() {
+    throw new Error("getFilterList not implemented");
+  }
+  getExtensionPreferences() {
+    throw new Error("getExtensionPreferences not implemented");
   }
 
   mangaFromElements(elements) {
